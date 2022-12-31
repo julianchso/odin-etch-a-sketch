be072ac70da615c3c1a7e1d674a9750b09d009cd
Julian So <ch.julian.so@gmail.com>
2022-12-31 10:08:47 -0500

    add updateGridSize to include text of size of grid

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ let colorPicker = document.querySelector("#colorPicker");
 let gridContainer = document.querySelector(".gridContainer");
 let gridSize = document.querySelector("#gridSize").value;
 
-console.log(gridSize);
+function updateGridSize(newGridSize) {
+  document.querySelector(
+    "#gridSizeLabel"
+  ).textContent = `${newGridSize.value} x ${newGridSize.value}`;
+  gridSize = newGridSize.value;
+  console.log(gridSize);
+  updateGrid(gridSize);
+}
 
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add(".gridSquare");
+function updateGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let newDiv = document.createElement("div");
+      newDiv.classList.add(".gridSquare");
 
-    gridContainer.appendChild(newDiv);
+      gridContainer.appendChild(newDiv);
+    }
   }
 }
