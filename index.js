8258ac22439daa3e2677fcb0335da8a4a3b3b243
Julian So <ch.julian.so@gmail.com>
2022-12-31 08:12:39 -0500

    add button and slider

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let colorPicker = document.querySelector("#colorPicker");
+let gridContainer = document.querySelector(".gridContainer");
+let gridSize = document.querySelector("#gridSize").value;
 
+console.log(gridSize);
 
+for (let i = 0; i < gridSize; i++) {
+  for (let j = 0; j < gridSize; j++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add(".gridSquare");
 
-console.log(colorPicker);
-
+    gridContainer.appendChild(newDiv);
+  }
+}
