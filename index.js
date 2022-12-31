cf9d91e185136adac627f52f1289f4425451b96d
Julian So <ch.julian.so@gmail.com>
2022-12-31 11:39:42 -0500

    add basic dynamic grid

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ function updateGridSize(newGridSize) {
   ).textContent = `${newGridSize.value} x ${newGridSize.value}`;
   gridSize = newGridSize.value;
   console.log(gridSize);
-  updateGrid(gridSize);
+  updateGrid();
 }
 
 function updateGrid() {
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
+  gridContainer.innerHTML = ""
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+  console.log(`updateGrid function: ${gridSize}`);
+  for (let i = 0; i <= gridSize; i++) {
+    for (let j = 0; j <= gridSize; j++) {
       let newDiv = document.createElement("div");
-      newDiv.classList.add(".gridSquare");
+      newDiv.classList.add("gridSquare");
 
       gridContainer.appendChild(newDiv);
     }
   }
+  console.log("Grid updated");
 }
